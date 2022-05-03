7883b812fb3817183abecb6b7ee56b78ba40afbc
dobin_shin <dobin_shin@tmax.co.kr>
2022-05-03 17:22:59 +0900

    [feat] 자기소개 입력

--- payload/introduce.ts
+++ payload/introduce.ts
@@ -1,15 +1,16 @@
 import { IIntroduce } from '../component/introduce/IIntroduce';
-import { lastestUpdatedAt } from '../package.json';
+import { lastestUpdatedAt, particpant } from '../package.json';
 
 const introduce: IIntroduce.Payload = {
   disable: false,
 
   contents: [
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer massa magna, feugiat commodo arcu sed, sagittis sollicitudin tortor. Nullam diam ex, interdum nec lacus vel, ultrices pretium sapien. Proin bibendum non nibh eu aliquet. In sit amet arcu sed nisi tincidunt ultricies. Fusce magna tellus, egestas at pretium quis, rutrum et ante. Phasellus efficitur lectus felis, sed lacinia enim rhoncus ut. Sed id mi vitae neque aliquet porttitor. Fusce dignissim ipsum urna, at finibus lectus sodales ut. Nulla ut purus vestibulum, dapibus tellus consequat, porttitor purus. Nunc egestas tortor ut vehicula mollis.',
-    'Integer ut libero eu libero mattis rutrum at at urna. Integer non turpis ante. Suspendisse vestibulum odio id libero facilisis gravida. In ipsum est, pellentesque vestibulum tortor eu, bibendum imperdiet metus. Quisque ornare eget ex ut congue. Nam vel finibus sem. Phasellus id neque nibh. Nullam turpis velit, pharetra vitae mi non, bibendum maximus urna.',
+    'Pay & Shop 2개 프로젝트를 기획부터 출시까지한 경험이 있습니다.',
+    'React Native, React, TypeScript, Electron, MobX, Git, Figma등을 주로 사용했습니다. MobX 패턴에 맞도록 컴포넌트를 성능 최적화한 경험, Shop 프로젝트에서 Promise 이용한 비동기 처리 경험이 있습니다.',
+    '저는 rough한 코드들이 스터디를 통해 최적화되어 가는 것을 좋아합니다. 움켜쥐지 않고 펴서 돕는 서비스를 개발하는 환경을 찾고 있습니다.',
   ],
-  sign: 'Love poem',
-  // sign: packageJson.author.name,
+  // sign: 'Love poem',
+  sign: particpant.name,
   // latestUpdated: '2019-07-24',
   latestUpdated: lastestUpdatedAt,
 };
